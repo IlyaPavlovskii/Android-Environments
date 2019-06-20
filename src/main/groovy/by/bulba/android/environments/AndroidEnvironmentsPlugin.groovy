f45c43711f361442d7d9f10f8dbdf54ba3d38606
Pavlovskii Ilya <pavlovskii_ilya@mail.ru>
2019-06-20 22:43:15 +0300

    Upgrade example application.

--- src/main/groovy/by/bulba/android/environments/AndroidEnvironmentsPlugin.groovy
+++ src/main/groovy/by/bulba/android/environments/AndroidEnvironmentsPlugin.groovy
@@ -1,9 +1,13 @@
 package by.bulba.android.environments
 
+import com.android.build.gradle.AppExtension
+import com.android.build.gradle.BaseExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.UnknownDomainObjectException
 
 class AndroidEnvironmentsPlugin implements Plugin<Project> {
+
     @Override
     void apply(Project project) {
         project.extensions
@@ -18,6 +22,23 @@ class AndroidEnvironmentsPlugin implements Plugin<Project> {
                 println("${ext.configPath}/${ext.configFile}")
                 println("buildTypes: ${ext.useBuildTypes}")
                 println("productFlavors: ${ext.useProductFlavors}")
+
+                try {
+                    def androidExtension = project.extensions.getByName("android")
+                    if( androidExtension instanceof BaseExtension) {
+                        androidExtension.buildTypes.forEach {
+                            println("buildType: ${it.name}")
+
+                        }
+                    }
+                    if (androidExtension instanceof AppExtension) {
+                        androidExtension.applicationVariants.forEach {
+                            println("appVariant: ${it.flavorName}")
+                        }
+                    }
+                } catch (UnknownDomainObjectException udoe) {
+                    println("android extension not found exception")
+                }
 
             }
         }
